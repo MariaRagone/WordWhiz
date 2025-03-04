a276c3aedb534e2fd68f81ffccd2059e09f96f78
mragone <maria.ragone@samaritanministries.org>
2025-03-04 10:36:10 -0500

    game is set up and styled

--- WordWhiz.js
+++ WordWhiz.js
@@ -1,41 +1,50 @@
 //promise chaining
 
-const doggo_url = "https://dog.ceo/api/breeds/image/random";
-console.log(doggo_url);
+const wordUrl = "https://words.dev-apis.com/word-of-the-day";
 
-const doggos = document.getElementById("dog-target");
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let randomNumber = getRandomInt(1, 100);
 
-const addNewDoggo = () => {
-  const promise = fetch(doggo_url);
-  promise
-    .then(function (result) {
-      const processingPromise = result.text();
-      return processingPromise;
-    })
-    .then(function (processedResult) {
-      const dogObject = JSON.parse(processedResult);
-      const img = document.createElement("img");
-      img.src = dogObject.message;
-      img.alt = "cute doggo";
-      doggos.appendChild(img);
-    })
-    .catch(function (error) {
-      console.log("there is an error");
-    });
+const randomWordUrl = `https://words.dev-apis.com/word-of-the-day?random=${randomNumber}`;
+console.log(randomWordUrl);
+
+const res = await fetch(randomWordUrl);
+const { word: wordRes } = await res.json();
+const word = wordRes.toUpperCase();
+const wordParts = word.split("");
+console.log(word);
+
+// const words = document.getElementById("word-target");
+
+// const getWord = async () => {
+//   const promise = await fetch(randomWordUrl);
+//   const processedResult = await promise.json();
+//   const word = document.createElement("word");
+//   word = processedResult.word;
+//   console.log(word);
+//   words.appendChild(word);
+// };
+
+// document.getElementById("get-word-btn").addEventListener("click", getWord);
+
+// const validateWord = async () => {
+//   const promise = await fetch(validateUrl, {
+//     method: 'POST',
+//     body: JSON.stringify({"word": "intent"})}
+// const processedResult = await promise.json()
+
+//   }
+
+const isLetter = (letter) => {
+  return /^[a-zA-Z]$/.test(letter);
 };
-document.getElementById("dog-btn").addEventListener("click", addNewDoggo);
 
-//async await method
-
-const doggos2 = document.getElementById("dog-target2");
-
-const addNewDoggo2 = async () => {
-  const promise = await fetch(doggo_url);
-  const processedResult = await promise.json();
-  const img = document.createElement("img");
-  img.src = processedResult.message;
-  img.alt = "cute doggo";
-  doggos2.appendChild(img);
-};
-
-document.getElementById("dog-btn2").addEventListener("click", addNewDoggo2);
+document
+  .querySelector(".tester-input")
+  .addEventListener("keydown", function (event) {
+    if (!isLetter(event.key)) {
+      event.preventDefault();
+    }
+  });
